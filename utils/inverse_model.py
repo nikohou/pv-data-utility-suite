1f0006246c6cf02d00be0310fc17383ba976ecbf
Nikolaus Houben <nikolaus.houben@gmail.com>
2022-08-04 13:34:35 +0200

    some new structure

---
+++ utils/inverse_model.py
@@ -0,0 +1,11 @@
+# angle_inference.py
+
+"""
+
+Find corresponding ghi, dni, dhi measurements for a given PV power timeseries. This module is an approximation of the pvwatts model with the following specifications:
+
+
+"""
+
+
+__all__ = []
