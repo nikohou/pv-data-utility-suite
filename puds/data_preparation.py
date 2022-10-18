b3b746b0b9faddf812bd447f0def6629abd0147b
Nikolaus Houben <nikolaus.houben@gmail.com>
2022-10-18 15:29:25 +0200

    progress on results

--- puds/data_preparation.py
+++ puds/data_preparation.py
@@ -31,3 +31,28 @@ def get_timestep_interval(df):
 
 def reindexer(df, ts_freq: int):
     return df.reindex(pd.date_range(df.index[0], df.index[-1], freq=str(ts_freq) + "S"))
+
+
+def pv_day_filter(data, site, tilt, azimuth, timesteplen):
+
+    index = data.index
+    times = pd.date_range(index[0], index[-1], freq=str(timesteplen) + "T")
+    clearsky = site.get_clearsky(times)
+    solar_position = site.get_solarposition(times=times)
+    # Use the get_total_irradiance function to transpose the GHI to POA
+    POA_irradiance = irradiance.get_total_irradiance(
+        surface_tilt=tilt,
+        surface_azimuth=azimuth,
+        dni=clearsky["dni"],
+        ghi=clearsky["ghi"],
+        dhi=clearsky["dhi"],
+        solar_zenith=solar_position["apparent_zenith"],
+        solar_azimuth=solar_position["azimuth"],
+    )
+
+    day_index = POA_irradiance[POA_irradiance["poa_global"] > 0].index
+
+    data_day_values = data.reindex(day_index).dropna()
+
+    return data_day_values
+
